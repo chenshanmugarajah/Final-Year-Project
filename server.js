e437a3b4145b4c01a9eda8019a94c34ac55c2b25
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-04-08 19:28:59 +0100

    Register page initial set up
    Setting up consent paths and checking the consents

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
 
-app.use(express.static(__dirname + '/public'));
-app.set('view-engine', 'ejs');
+app.use(express.static(__dirname + '/public'))
+app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 })
 
-app.listen(3000, console.log('Server started on port 3000'));
+app.get('/register', (req, res) => {
+ res.render('register.ejs');
+})
+
+app.listen(3000, console.log('Server started on port 3000'))
