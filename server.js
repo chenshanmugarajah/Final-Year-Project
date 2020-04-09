13418499305e402dac366f3705f2fe0edce89c4c
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-04-09 11:20:41 +0100

    Get routes
    Set up get routes for login and register

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express')
 const app = express()
+const bcrypt = require('bcryptjs')
+
+// change to database
+const users = []
 
 app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
   res.render('index.ejs')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+  } catch {
+    
+  }
 })
 
 app.listen(3000)
