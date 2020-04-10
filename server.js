ca765b35657b7dd4246f46a344970b2f4dd100c5
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-04-10 13:10:59 +0100

    Login and register
    setting up login and register functionality

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const app = express()
 const bodyParser = require('body-parser')
 const bcrypt = require('bcryptjs')
 
+// app set up
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.listen(3000)
-
-// change to database
-const users = []
-
 app.set('view-engine', 'ejs')
 
+// server
+app.listen(3000)
+
+const AWS = require('aws-sdk')
+AWS.config.update({
+  region: 'us-east-1',
+  endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
+})
+const documentClient = new AWS.DynamoDB.DocumentClient()
+
+// routes
 app.get('/', (req, res) => {
   res.render('index.ejs')
 })
@@ -21,11 +27,47 @@ app.get('/login', (req, res) => {
   res.render('login.ejs')
 })
 
+app.post('/login', (req, res) => {
+  let password = req.body.password
+  let username = req.body.username
+
+  let params = {
+    TableName: 'RedUsers',
+    Key: {
+      username: username,
+      password: password
+    }
+  }
+
+  let user = '';
+
+  documentClient.get(params, (err, data) => {
+    if (err) console.log(err)
+    else {
+      console.log(data.size)
+    }
+  })
+
+})
+
 app.get('/register', (req, res) => {
   res.render('register.ejs')
 })
 
 app.post('/register', (req, res) => {
   let password = req.body.password
-  console.log(password)
+  let username = req.body.username
+
+  let params = {
+    TableName: 'RedUsers',
+    Item: {
+      username: username,
+      password: password
+    }
+  }
+
+  documentClient.put(params, (err, data) => {
+    if (err) console.log(err)
+    else res.redirect('/login')
+  })
 })
