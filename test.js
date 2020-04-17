aa042148cb83491efda33616c2cfb5f5fc47932d
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-04-17 14:24:59 +0100

    Practice functions
    Test.js practice

--- test.js
+++ test.js
@@ -1,28 +1,92 @@
-let users = {"111":{"explane":2,"question":1,"goback":1,"louder":0,"slower":0,"faster":0,"clearer":0,"breaktime":3},"test":{"explane":2,"question":3,"goback":0,"louder":0,"slower":3,"faster":0,"clearer":0,"breaktime":0}}
+let users = {
+  "chen": { 
+    "explane":2,"question":1,"goback":1,"louder":0,"slower":0,"faster":0,"clearer":0,"breaktime":3
+  },
+  "test": {
+    "explane":2,"question":3,"goback":0,"louder":0,"slower":3,"faster":0,"clearer":0,"breaktime":0
+  }
+}
 
 console.log('\nTest file\n')
 
-let votes = {}
+let collatedVotes = {}
+let usersList = {}
+let usersTotalVotes = {}
 
-function sortData () {
+let ts = 0;
 
-  for(let user in users) {
-    let userData = users[user]
-    delete userData.username
+// collatedVotes[ts] = {
+//   votes: {
+//     explane: 1,
+//     faster: 2
+//   },
+//   mostVoted: "faster",
+//   intVotes: 3
+// }
 
-    for (let data in userData){
-      if(votes[data] == undefined) {
-        votes[data] = 0;
-      }
+// usersList[ts] = {
+//   username: {
+//     votes: {
+//       explane: 1,
+//       faster: 2
+//     },
+//     mostVoted: 'faster',
+//     totalVotes: 3
+//   }
+// }
+
+function getUsers (users) {
+  usersList[ts] = []
+  collatedVotes[ts] = []
+  usersTotalVotes[ts] = 0
+  for (let user in users) {
+    usersList[ts][user] = {
+      votes: users[user],
+      mostVoted: mostVoted(users[user]),
+      totalVotes: totVotes(users[user])
     }
-
-    for (let data in userData){
-      votes[data] += userData[data]
-    }
+    collateVotes(user)
   }
-
-  console.log(votes)
-
+  console.log(usersList)
+  console.log(usersTotalVotes)
+  ts += 5
 }
 
-sortData()
+function collateVotes(user) {
+  for (let data in user) {
+    collatedVotes[ts] 
+  }
+}
+
+function totVotes (user) {
+  let total = 0;
+  for(let data in user) {
+    total += user[data]
+  }
+  usersTotalVotes[ts] += total
+  return total
+}
+
+function mostVoted (user) {
+  let dataArr = []
+  for(let data in user) {
+    dataArr.push({
+      name: data,
+      value: user[data]
+    })
+  }
+  dataArr.sort(function (a, b) {
+    if (a.value > b.value) {
+      return -1
+    } else if (b.value > a.value) {
+      return +1
+    } else {
+      return 0
+    }
+  })
+  return dataArr[0].name
+}
+
+setInterval(() => {
+  getUsers(users)
+}, 5000);
