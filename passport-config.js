35b8b19f5a9d1238ba6b1ae3d2414994db42bbbd
Chen Shanmugarajah <chenshanmugarajah@gmail.com>
2020-04-28 22:35:37 +0100

    Commented completed
    Comments completed and code neatened

--- passport-config.js
+++ passport-config.js
@@ -1,14 +1,19 @@
+//Consts
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
 
+//Initialising passport module to store data in session
 function initializePassport(passport, getUserByUsername) {
-    
     const authenticateUser = async (username, password, done) => {
+        //Get user from dynamodb
         const user = (await getUserByUsername(username)).Items[0]
 
+        //Error handling
         if(typeof user == "undefined") {
             return done(null, false, { message: 'No user with that username' })
         }
+
+        //Password check
         try {
             if(await bcrypt.compare(password, user.password)) {
                 return done(null, user)
@@ -20,11 +25,15 @@ function initializePassport(passport, getUserByUsername) {
         }
     }
 
+    //Set passport strategy
     passport.use(new LocalStrategy({ usernameField: 'username', password: 'password'}, authenticateUser))
+    
+    //Log on and log off handling
     passport.serializeUser((user, done) => done(null, user.username))
     passport.deserializeUser((username, done) => {
         return done(null, getUserByUsername(username))
     })
 }
 
+//Exporting the function
 module.exports = initializePassport
--- server.js
+++ server.js
@@ -38,9 +38,10 @@ AWS.config.update({
   region: 'us-east-1',
   endpoint: 'https://dynamodb.us-east-1.amazonaws.com'
 })
+//DocumentClient created to store data
 const documentClient = new AWS.DynamoDB.DocumentClient()
 
-// routes
+//GET routes
 app.get('/', checkAuthenticated, async (req, res) => {
   const user = (await req.user).Items[0]
   res.render('index.ejs', {username : user.username} )
@@ -58,6 +59,29 @@ app.get('/register', checkNotAuthenticated, checkConsent, (req, res) => {
   }
 })
 
+app.get('/data-consent', (req, res) => {
+  res.render('data-consent.ejs')
+})
+
+app.get('/participation-consent', (req, res) => {
+  res.render('data.participation.ejs')
+})
+
+app.get('/voting', checkAuthenticated, async (req, res) => {
+  const user = (await req.user).Items[0]
+  res.render('voting.ejs', {username: user.username})
+})
+
+app.get('/emotion', checkAuthenticated, async (req, res) => {
+  const user = (await req.user).Items[0]
+  res.render('emotion.ejs', {username: user.username})
+})
+
+app.get('/results', checkAuthenticated, (req, res) => {
+  res.render('results.ejs')
+})
+
+//POST routes
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
@@ -106,34 +130,72 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
 
 })
 
-// get user from dynamodb
+app.post('/data-consent', (req, res) => {
+  consent.data = true;
+  res.redirect('/participation-consent')
+})
+
+app.post('/participation-consent', (req, res) => {
+  consent.participation = true;
+  console.log(consent)
+  res.redirect('/register')
+})
+
+app.post('/emotion', (req, res) => {
+  console.log('posting accepted from server.js')
+})
+
+//Route override for logout
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  consent.data = false
+  consent.participation = false
+  res.redirect('/login')
+})
+
+//Server
+app.listen(3000, () => {
+  console.log('Server started: http://localhost:3000')
+})
+
+//Get user from dynamodb
 async function getUser(username) {
   let params = {
     TableName: 'RedUsers',
     FilterExpression: 'username = :username',
     ExpressionAttributeValues: {':username' : username}
   }
-
   let data  = await documentClient.scan(params, (err, data) => {
     if (err) console.log(err)
   }).promise()
 
+  //Return data as a promise
   return data;
 }
 
+//Check if logged in, to disallow pages
 function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   }
-
   res.redirect('/login')
 }
 
+//Check if not logged in, to disallow login and register pages
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+//Consent 
 let consent = {
   data: false,
   participation: false
 }
 
+//Checking consent has been given
 function checkConsent(req, res, next) {
   if(consent.data == false) {
     res.redirect('/data-consent')
@@ -143,58 +205,3 @@ function checkConsent(req, res, next) {
     return next();
   }
 }
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect('/')
-  }
-  next()
-}
-
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  consent.data = false
-  consent.participation = false
-  res.redirect('/login')
-})
-
-app.get('/voting', checkAuthenticated, async (req, res) => {
-  const user = (await req.user).Items[0]
-  res.render('voting.ejs', {username: user.username})
-})
-
-app.get('/data-consent', (req, res) => {
-  res.render('data-consent.ejs')
-})
-
-app.post('/data-consent', (req, res) => {
-  consent.data = true;
-  res.redirect('/participation-consent')
-})
-
-app.get('/participation-consent', (req, res) => {
-  res.render('data.participation.ejs')
-})
-
-app.post('/participation-consent', (req, res) => {
-  consent.participation = true;
-  console.log(consent)
-  res.redirect('/register')
-})
-
-app.get('/results', checkAuthenticated, (req, res) => {
-  res.render('results.ejs')
-})
-
-app.get('/emotion', checkAuthenticated, async (req, res) => {
-  const user = (await req.user).Items[0]
-  res.render('emotion.ejs', {username: user.username})
-})
-
-app.post('/emotion', (req, res) => {
-  console.log('posting accepted from server.js')
-})
-// server
-app.listen(3000, () => {
-  console.log('Server started: http://localhost:3000')
-})
